83d46ac1f280732e6154e1cf1168e58c2a9b610b
Entrimail <57843883+Entrimail@users.noreply.github.com>
2022-05-31 14:32:57 +0300

    Update generator.py

--- flask_app/generator.py
+++ flask_app/generator.py
@@ -1,13 +1,10 @@
 import random
-from tkinter import N
 
 
 def generate_password():
-    N = random.randint(8, 16) 
-  
+    n = random.randint(8, 16) 
     psw = ''
-    for i in range (N):
-            
-        psw = psw + random.choice(list('1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+    for i in range (n):            
+        psw += random.choice(list('1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
 
     return f'{psw}'
